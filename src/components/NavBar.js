bcd7db7dc4eed65fc27a907de7b10693200c19e6
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-10-05 09:52:41 +0300

    Add and style the NavBar section

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar(){
     return(
     <div>
-       
+        <nav>
+            <h2>BoOk LOver</h2>
+            <a href="#Home">Home</a>
+            <a href="#Book">Book</a>
+            <a href="#Catalogue">Catalogue</a>
+        </nav>
 
     </div>
     );
