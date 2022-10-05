d707e0d86b453797c48722f1b4ae741460406cc6
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-10-05 10:22:53 +0300

    style the nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ function NavBar(){
         <nav>
             <h2>BoOk LOver</h2>
             <a href="#Home">Home</a>
-            <a href="#Book">Book</a>
+            <a href="#Books">Books</a>
             <a href="#Catalogue">Catalogue</a>
         </nav>
 
