98053444a41b38cf6515bd07aa53d4e76e40ac70
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-10-08 23:06:35 +0300

    style home component

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 function Home(){
     return(
     <div>
-        <h1>“Books are mirrors:<br></br>you only see in them what you already have inside you.” </h1>
+        <H1>“Books are mirrors:<br></br>you only see in them what you already have inside you.” </H1>
 
          <ImgBody>
               <img src={book_lover} alt="book lover" width="400px" />
@@ -22,13 +22,31 @@ const ImgBody = styled.div`
    
   
 
-      img {
+        img {
      display: flex;
      margin-left: auto;
      margin-right: auto;
     width: 40%;
      }
 
+`
+const Button = styled.button`
+         background-color:blue
+         padding: 20px;
+         position: absolute;
+         cursor: pointer; 
+         left: 42%;
+         margin: 50px 10px;
+         border-radius:5%;
+         font-size: larger;
+`
+const H1 = styled.h1`
+         text-align: center;
+         color: indigo;
+         font-style: italic;
+         font-weight: lighter;
+         font-family:Cormorant Upright ;
+         font-style: italic;
 `
 
  
