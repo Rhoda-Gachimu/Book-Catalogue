357da1be545af17f100d1fca3df4ba837c7b6d14
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-10-09 14:21:17 +0300

    state,events and data fetching

--- src/components/BookCategories.js
+++ src/components/BookCategories.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState } from "react"
 
-function BookCategories(){
-return(
-    <>
-   <button>Comic Books</button>
-   <button>Action_Adventure</button>
-   <button>Detective_Mystery</button>
-   <button>Classics</button>
-   <button>Fantasy</button>
+function BookCategories() {
+  
+   const [books, setBooks] = useState([]);
+  
 
-    </>
-)
+  const fetchBook= (category)=>{
+    fetch(`http://localhost:3000/${category}`)
+    .then((response)=>response.json())
+    .then((books)=>{
+      setBooks(books);
+    });
 
+  }
+
+  const handleClick = (event)=>{
+    fetchBook(event.target.value)
+  }
+  
+  return (
+    <div>
+
+      <button onClick={handleClick} value="comicbooks">Comic Books</button>
+      
+      <button onClick={handleClick} value="Action_Adventure" >Action_Adventure</button>
+      <button onClick={handleClick} value="Detective_Mystery">Detective_Mystery</button>
+      <button onClick={handleClick} value="Classics">Classics</button>
+      <button onClick={handleClick} value="Fantasy">Fantasy</button>
+      <ol>
+            <li key={books.index}> {books.map((book)=>book.title)} </li>
+      </ol>
+    </div>
+  );
 }
-
 
 export default BookCategories;
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,24 +1,10 @@
 import React from "react";
 
-function BookList(){
-  //   const [books, setBooks] = useState([])
 
-
-  // fetch("http://localhost:8000/comicbooks")
-  // .then((data) => data.json())
-  // .then((newData) => setBooks(newData))
-
-    return(
-    <div>
-<h1>Books Available</h1>
-      <ol>
-        <li></li>
-        
-      </ol>
-    </div>
-    )
+function BookList() {
+   return(
+    <div></div>
+   )
 }
-  
-
 
 export default BookList;
